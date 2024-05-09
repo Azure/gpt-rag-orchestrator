cde081f4150deb94586cb693ea9a9c11ee1abf8c
Manuel Castro <maacaro@gmail.com>
2024-05-09 09:51:14 -0400

    Revert "SFC-172 langchain conversation memory (#48)" (#49)
    This reverts commit ba7587f3a51775a148e69f87c94ed032997aa7f1.

--- orc/code_orchestration.py
+++ orc/code_orchestration.py
@@ -2,14 +2,13 @@
 import logging
 import os
 from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import SystemMessage
 from langchain.prompts import ChatPromptTemplate
 from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, ConversationChain
+from langchain.prompts import HumanMessagePromptTemplate
+from langchain.chains import LLMChain
 from shared.tools import LineListOutputParser, retrieval_transform
 from langchain_community.retrievers import AzureAISearchRetriever
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 
 # logging level
 
@@ -36,7 +35,7 @@ AZURE_OPENAI_TOP_P = os.environ.get("AZURE_OPENAI_TOP_P") or "0.27"
 AZURE_OPENAI_TOP_P = float(AZURE_OPENAI_TOP_P)
 AZURE_OPENAI_RESP_MAX_TOKENS = os.environ.get("AZURE_OPENAI_MAX_TOKENS") or "1000"
 AZURE_OPENAI_RESP_MAX_TOKENS = int(AZURE_OPENAI_RESP_MAX_TOKENS)
-CONVERSATION_MAX_HISTORY = os.environ.get("CONVERSATION_MAX_HISTORY") or "12"
+CONVERSATION_MAX_HISTORY = os.environ.get("CONVERSATION_MAX_HISTORY") or "3"
 CONVERSATION_MAX_HISTORY = int(CONVERSATION_MAX_HISTORY)
 
 AZURE_OPENAI_API_KEY = os.environ.get("AZURE_OPENAI_API_KEY")
@@ -47,7 +46,6 @@ AZURE_OPENAI_CHAT_DEPLOYMENT_NAME = "chat"
 ORCHESTRATOR_FOLDER = "orc"
 PLUGINS_FOLDER = f"{ORCHESTRATOR_FOLDER}/plugins"
 BOT_DESCRIPTION_FILE = f"{ORCHESTRATOR_FOLDER}/bot_description.prompt"
-
 
 
 # replaced by format_messages in the orchestrator
@@ -93,50 +91,22 @@ Original question: {question}"""
     return retriever
 
 
-def get_conversation_chain(model, messages, context):
-    memory = ConversationBufferWindowMemory(k=CONVERSATION_MAX_HISTORY)
-
-    input, output = {}, {}
-    for message in messages:
-        if message.type == "human":
-            input["input"] = message.content
-        if message.type == "ai":
-            output["output"] = message.content
-        if "input" in input and "output" in output:
-            memory.save_context(input, output)
-            input, output = {}, {}
-
-    prompt_template = """You are FreddAid, a friendly marketing assistant dedicated to uncovering insights and developing effective strategies. 
-    Answer the following question based on the current context and conversation:
-    Context: ```{{context}}```
-    Make sure you cite the source number as [x]. Do not add the word Source before the number.
-    Conversation History: ```{history}```
-    Question: ```{input}```
-    """
-
-    # insert the context into the prompt
-    prompt_template = prompt_template.replace("{{context}}", context)
-
-    prompt = PromptTemplate.from_template(prompt_template)
-
-    conversation_chain = ConversationChain(llm=model, memory=memory, prompt=prompt)
-
-    # logging.info(
-    #     f"MEMORY history: {conversation_chain.memory.load_memory_variables(inputs=[])['history']}"
-    # )
-
-    return conversation_chain
-
-
 async def get_answer(question, messages, settings):
     answer_dict = {}
     total_tokens = 0
     try:
         model = AzureChatOpenAI(
-            temperature=settings["temperature"],
+            temperature=settings['temperature'],
             openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
             azure_deployment=os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"],
         )
+        
+        if len(messages) == 0:
+            messages = [
+                SystemMessage(
+                    content="You are FreddAid, a friendly marketing assistant dedicated to uncovering insights and developing effective strategies."
+                ),
+            ]
 
         # get document retriever and create retrieval chain
         retriever = get_document_retriever(model)
@@ -144,37 +114,29 @@ async def get_answer(question, messages, settings):
 
         # get source knowledge from retrieval chain documents
         source_knowledge, sources = retrieval_chain.invoke(question)
-
-        # get conversation chain then format messages and context
-        conversation_bufw = get_conversation_chain(
-            model, messages, context=source_knowledge
+        humanMessage = HumanMessagePromptTemplate.from_template(
+            """Answer the following question based on this context:\n{context}\nQuestion: {question}\n Make sure you cite the source number as [x]. Do not add the word Source before the number."""
         )
 
-        # get response from conversation chain
-        # returns: input -> question, history, response -> answer
-        res = conversation_bufw.invoke(input=question)
+        # format the message into a human message and append to messages
+        humanMessage = humanMessage.format_messages(
+            context=source_knowledge, question=question
+        )
+        messages.append(humanMessage[0])
 
-        # logging.info(f"response: {res}")
-
-        # human question
-        humanMessage = HumanMessage(content=question)
-        messages.append(humanMessage)
-        # ai response
-        ai_message = AIMessage(content=res["response"])
-        messages.append(ai_message)
-
+        # get the response from the model
+        res = model.invoke(messages)
+        messages.append(res)
 
         answer_dict["answer"] = replace_numbers_with_paths(res.content, sources)
         answer_dict["ai_message"] = res
         answer_dict["human_message"] = humanMessage[0]
-
         answer_dict["total_tokens"] = total_tokens
         answer_dict["sources"] = sources
 
     except Exception as e:
         logging.error(f"[code_orchest] exception when executing RAG flow. {e}")
         answer_dict["answer"] = f"RAG flow: exception: {e}"
-        answer_dict["sources"] = []
 
     answer_dict["total_tokens"] = total_tokens
     answer_dict["user_ask"] = question
