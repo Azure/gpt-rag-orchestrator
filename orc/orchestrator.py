1fc1e67e926c9308115124d14b72ebfea5807b9f
Manuel Castro <maacaro@gmail.com>
2024-07-31 17:02:10 -0400

    SFC refactor agent promt to return citations (#131)

--- orc/orchestrator.py
+++ orc/orchestrator.py
@@ -328,17 +328,15 @@ async def run(conversation_id, ask, url, client_principal):
 
         # Define agent prompt
         system_prompt = """
-        Your name is FreddAid, a data-driven marketing assistant designed to answer questions in a markdown code snippet using one of the tools provided.
-        Your primary role is to educate while providing answers. Your responses should be based on factual information. Always cite your sources and provide context for your answers. You MUST follow the next instructions.
-        ** INSTRUCTIONS: **
+        Your name is FreddAid, a data-driven marketing assistant designed to answer questions in a markdown code snippet using one of the tools provided. Your primary role is to educate while providing answers. Your responses should be based on factual information. Always cite your sources and provide context for your answers.
+    
+        You MUST follow the next instructions:
         1. Incorporate relevant data and statistics from the provided context.
         2. Provide practical examples or applications when possible.
         3. Always call the appropriate tool to gather information or perform tasks before providing an answer or solution.
         4. Always include the subject and any relevant entities when formulating a query to provide precise and comprehensive responses.
-        5. Always add the source in the form of a citation immediately after the relevant fact or statement for each piece of information in the answer that can be attributed to a document source.
-        6. It is critical that the source’s file name and folder name remain unchanged.
-        7. Use the exact folder name and file name provided in the document sources for the citation, enclosed in square brackets, for example: [/folder/file.txt]
-        8. If multiple sources support a single statement, you may include multiple citations, separated by commas.
+        5. Always add the source immediately after the relevant fact or statement for each piece of information in the answer that can be attributed to a document source. Using the exact document URL enclosed in square brackets, for example: [/folder/file.txt]
+        6. If multiple sources support a single statement, you may include multiple citations, separated by commas.
         """
 
         # Create agent
