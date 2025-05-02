62d5b1dc94746f1933c61467b9d24afae214a21a
Chris Tava <christava@microsoft.com>
2025-05-01 23:52:07 -0400

    chg: isWorkMode-3

--- orc/code_orchestration.py
+++ orc/code_orchestration.py
@@ -58,6 +58,11 @@ async def get_answer(history, security_ids,conversation_id, is_work_mode):
     # INITIALIZATION
     #############################
 
+    #initialize variables    
+    global BING_RETRIEVAL
+    global SEARCH_RETRIEVAL
+    global RETRIEVAL_PRIORITY 
+
     # work mode:
     #  true - prioritize search retrieval, turn off bing search
     # false - prioritize bing retrieval, turn off search retrieval
@@ -70,7 +75,6 @@ async def get_answer(history, security_ids,conversation_id, is_work_mode):
             RETRIEVAL_PRIORITY = "bing"
         SEARCH_RETRIEVAL = False
 
-    #initialize variables
     answer_dict = {}
     prompt = "The prompt is only recorded for question-answering intents"
     answer = ""
