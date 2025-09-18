e62d14c632069120b51a54f7ca1322092f23af33
Nam Tran <nam3864779@gmail.com>
2025-09-18 12:06:24 -0400

    fix(mcp_executor): update query reference for web fetch tool call (#496)

--- orc/graphs/mcp_executor.py
+++ orc/graphs/mcp_executor.py
@@ -101,7 +101,7 @@ class MCPExecutor:
         if tool_call["name"] == TOOL_WEB_FETCH:
             tool_call["args"].update(
                 {
-                    "query": state.state.question,
+                    "query": state.question
                 }
             )
 
